13f628521179dc6d3f463812260246e2cca84066
mali295 <47615416+mali295@users.noreply.github.com>
2021-03-06 13:18:19 -0500

    Added boundaries for spaceship

--- mali295_SE2250_assignment2/Assets/Scripts/Player Scripts/BoundsCheck.cs
+++ mali295_SE2250_assignment2/Assets/Scripts/Player Scripts/BoundsCheck.cs
@@ -18,26 +18,35 @@ public class BoundsCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
     }
 
     void MovePlayer() {
-        if(Input.GetAxisRaw("Vertical") > 0f) {
+        // for loop is used when the space is above 0 and if no key is pressed it will stay at 0
+        if (Input.GetAxisRaw("Vertical") > 0f) {
+
              Vector3 temp = transform.position;
              temp.y += speed * Time.deltaTime;
 
-             if(temp.y > max_Y) {
+            // I am putting this for loop to show that the value cannot be greated than the 4.3 that has been set and if it is greater it will be set at 4.3
+             if (temp.y > max_Y) 
                  temp.y = max_Y; 
-             } 
+             
 
              transform.position = temp;
+        
+        // for loop is used when the space is below 0 and if no key is pressed it will stay at 0
         } else if (Input.GetAxisRaw("Vertical") < 0f) {
+
             Vector3 temp = transform.position;
+            // using Time.deltaTime so it can move slowly/smoothly
             temp.y -= speed * Time.deltaTime;
 
-             if(temp.y < min_Y) {
+
+            // I am putting this for loop to show that the value cannot be greated than the -4.3 that has been set and if it is smaller then the value will be set at -4.3 
+             if (temp.y < min_Y) 
                  temp.y = min_Y; 
-             } 
+             
 
             transform.position = temp;
         }
