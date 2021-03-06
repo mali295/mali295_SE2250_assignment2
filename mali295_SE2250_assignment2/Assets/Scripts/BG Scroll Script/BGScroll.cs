82783c3d9888ec584191dba2decdcf6e2b01b22b
mali295 <47615416+mali295@users.noreply.github.com>
2021-03-06 12:31:28 -0500

    Changed the background to space backgrond

---
+++ mali295_SE2250_assignment2/Assets/Scripts/BG Scroll Script/BGScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    // speed of the scroll
+    public float scroll_Speed = 0.1f;
+    // this is where we hold the material in order to scroll
+    private MeshRenderer mesh_Renderer;
+
+    private float x_Scroll;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+    }
+
+    void Scroll() {
+        // time.time is the time since we started the game
+        x_Scroll = Time.time * scroll_Speed;
+
+        Vector2 offset = new Vector2(x_Scroll, 0f);
+        // pass the name of the texture
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+
+}
