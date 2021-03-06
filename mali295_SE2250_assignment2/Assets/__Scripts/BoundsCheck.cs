9c5025b772421a4ad2e4446035e30b41d53fa24e
mali295 <47615416+mali295@users.noreply.github.com>
2021-03-06 14:40:53 -0500

    Worked on Hero Script
    allowed the hero ship to move side to side but it still leaves the screen

---
+++ mali295_SE2250_assignment2/Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+
+/// <summary>
+/// Keeps a GameObject on screen
+/// Note that this Only works for an orthographic Main camera [0,0,0]
+/// <summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
