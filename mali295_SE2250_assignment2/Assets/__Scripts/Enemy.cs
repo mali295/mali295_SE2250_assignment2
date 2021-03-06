ad2abb07abd64482716af649130a469a5d649b83
mali295 <47615416+mali295@users.noreply.github.com>
2021-03-06 15:25:39 -0500

    Worked on Enemy script

---
+++ mali295_SE2250_assignment2/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;       // The speed in m/s
+    public float fireRate = 0.3f;  // Seconds/shot 
+    public float health = 10;     // enemy health
+    public int score = 100       // Points earned for destroying this
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
